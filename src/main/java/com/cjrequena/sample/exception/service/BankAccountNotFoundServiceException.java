a498ae317a802426fa048efa9b66270371e6301f
cjrequena <requenacj@gmail.com>
2022-03-10 09:49:33 +0100

    Added QueryService and QueryController

---
+++ src/main/java/com/cjrequena/sample/exception/service/BankAccountNotFoundServiceException.java
@@ -0,0 +1,13 @@
+package com.cjrequena.sample.exception.service;
+
+/**
+ *
+ * <p></p>
+ * <p></p>
+ * @author cjrequena
+ */
+public class BankAccountNotFoundServiceException extends ServiceException {
+  public BankAccountNotFoundServiceException(String message) {
+    super(message);
+  }
+}
--- src/main/java/com/cjrequena/sample/mapper/BankAccountMapper.java
+++ src/main/java/com/cjrequena/sample/mapper/BankAccountMapper.java
@@ -71,4 +71,6 @@ public interface BankAccountMapper {
 
   BankAccountEntity toEntity(BankAccountDTO dto);
 
+  BankAccountDTO toDTO(BankAccountEntity entity);
+
 }
